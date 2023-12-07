6edab480e13055f21814657b58095ede86f55df0
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-12-07 18:18:01 +0530

    button

--- client/src/components/common/CommonButton.jsx
+++ client/src/components/common/CommonButton.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CommonButton = ({ to, onClick, className, children }) => {
-  const commonProps = {
+  const props = {
     className: `py-2 ${className}`,
   };
 
   if (to) {
     return (
-      <Link to={to} {...commonProps}>
+      <Link to={to} {...props}>
         {children}
       </Link>
     );
   } else if (onClick) {
     return (
-      <button onClick={onClick} {...commonProps}>
+      <button onClick={onClick} {...props}>
         {children}
       </button>
     );
