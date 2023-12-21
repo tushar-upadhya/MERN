f190d30656b0f4a86f96e2435fe375812eef6dd2
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-12-21 22:08:55 +0530

    connection refused

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
+import path from "path";
 import express from "express";
+import dotnet from "dotenv";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+
+dotnet.config();
+const port = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log("PORT:", PORT);
+app.get("/", (req, res) => {
+  res.send("heelo");
+});
+
+app.listen(port, () => {
+  console.log("port:", port);
 });
