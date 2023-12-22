44bbc73ca45a9418eac2e95ffdad7ddca2781382
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-12-22 11:13:45 +0530

    modules

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 
 dotnet.config();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
